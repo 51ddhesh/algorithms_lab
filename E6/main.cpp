d4c2a7bcf0696e6577b3043304a85c9391359118
Siddhesh Badnapurkar <siddheshbadnapurkar@gmail.com>
2025-04-15 07:30:32 +0530

    Update main.cpp

--- E6/main.cpp
+++ E6/main.cpp
@@ -1,66 +1,90 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int INF = numeric_limits<int>::max();
+int V; 
+vector<vector<pair<int, int>>> mst;
 
-struct City {
-    int x, y;
-};
-
-double distance(const City& a, const City& b) {
-    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+void dfs(int u, vector<bool>& visited, vector<int>& preorder) {
+    visited[u] = true;
+    preorder.push_back(u);
+    for (auto& [v, w] : mst[u]) {
+        if (!visited[v]) {
+            dfs(v, visited, preorder);
+        }
+    }
 }
 
-double nearestNeighborTSP(const vector<City>& cities, vector<int>& path) {
-    int n = cities.size();
-    vector<bool> visited(n, 0);
-    visited[0] = 1;
-    int currentCity = 0;
-    double totalDistance = 0.0;
-    path.push_back(currentCity);
-    for (int i = 1; i < n; ++i) {
-        double minDist = numeric_limits<double>::infinity();
-        int nextCity = -1;
-        for (int j = 0; j < n; ++j) {
-            if (!visited[j]) {
-                double dist = distance(cities[currentCity], cities[j]);
-                if (dist < minDist) minDist = dist, nextCity = j;
+int primMST(const vector<vector<int>>& graph) {
+    vector<int> key(V, INF);
+    vector<bool> inMST(V, false);
+    vector<int> parent(V, -1);
+    key[0] = 0;
+    int mst_weight = 0;
+
+    for (int count = 0; count < V - 1; ++count) {
+        int u = -1, minKey = INF;
+
+        for (int v = 0; v < V; ++v)
+            if (!inMST[v] && key[v] < minKey)
+                minKey = key[v], u = v;
+
+        inMST[u] = true;
+
+        for (int v = 0; v < V; ++v) {
+            if (graph[u][v] && !inMST[v] && graph[u][v] < key[v]) {
+                key[v] = graph[u][v];
+                parent[v] = u;
             }
         }
-        visited[nextCity] = 1;
-        path.push_back(nextCity); 
-        totalDistance += minDist, currentCity = nextCity;
     }
 
-    totalDistance += distance(cities[currentCity], cities[0]);
-    path.push_back(0); 
-    return totalDistance;
-}
-
-double calculateTourLength(const vector<int>& tour, const vector<City>& cities) {
-    double totalLength = 0;
-    for (size_t i = 0; i < tour.size() - 1; ++i) {
-        totalLength += distance(cities[tour[i]], cities[tour[i + 1]]);
+    mst.assign(V, vector<pair<int, int>>());
+    for (int i = 1; i < V; ++i) {
+        int u = parent[i];
+        int v = i;
+        int w = graph[u][v];
+        mst[u].emplace_back(v, w);
+        mst[v].emplace_back(u, w);
+        mst_weight += w;
     }
-    totalLength += distance(cities[tour.back()], cities[tour[0]]);
-    return totalLength;
+
+    return mst_weight;
 }
 
-double calculateApproximationError(double approx, double optimal) {
-    return (approx - optimal) / optimal;
+int calculateTourWeight(const vector<int>& tour, const vector<vector<int>>& graph) {
+    int weight = 0;
+    for (int i = 0; i < tour.size() - 1; ++i) {
+        weight += graph[tour[i]][tour[i + 1]];
+    }
+    weight += graph[tour.back()][tour[0]];
+    return weight;
 }
 
 int main() {
-    vector<City> cities = {
-        {0, 0}, {1, 2}, {2, 4}, {3, 1}, {5, 0}
-    };
-    vector<int> path;
-    double approxSolution = nearestNeighborTSP(cities, path);
-    double optimalSolution = 10.0;
-    double error = calculateApproximationError(approxSolution, optimalSolution);
-    cout << "Approximate Solution: " << approxSolution << '\n';
-    cout << "Optimal Solution: " << optimalSolution << '\n';
-    cout << "Error of Approximation: " << error * 100 << "%" << '\n';
-    cout << "Path Traveled: ";
-    for (int cityIndex : path) cout << cityIndex << " ";
-    cout << '\n';
+    cin >> V;
+
+    vector<vector<int>> graph(V, vector<int>(V));
+    cout << "Enter adjacency matrix:\n";
+    for (int i = 0; i < V; ++i)
+        for (int j = 0; j < V; ++j)
+            cin >> graph[i][j];
+
+    int mst_weight = primMST(graph);
+
+    vector<bool> visited(V, false);
+    vector<int> preorder;
+
+    dfs(0, visited, preorder);
+    preorder.push_back(0); 
+    int tour_weight = calculateTourWeight(preorder, graph);
+
+    double approximation_ratio = (double)tour_weight / mst_weight;
+
+    cout << "\nPreorder TSP Tour:\n";
+    for (int v : preorder) cout << v << " ";
+    cout << "\nTour weight: " << tour_weight;
+    cout << "\nMST weight: " << mst_weight;
+    cout << "\nApproximation ratio: " << approximation_ratio << endl;
+
     return 0;
 }
